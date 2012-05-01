a7bbf0b8773d828a88ff41511ea6f8a82e77565e
Kyo Lee <kyo.lee@eucalyptus.com>
2012-05-01 15:18:45 -0700

    using euca2ools-2.0 instead

--- stage01/download_euca2ools.sh
+++ stage01/download_euca2ools.sh
@@ -4,20 +4,20 @@ dir=${PWD}
 if ls -la | grep boto; then
   rm -fr  ./boto
 fi
-if ls -la | grep euca2ools-main; then
-  rm -fr ./euca2ools-main
+if ls -la | grep euca2ools-2.0; then
+  rm -fr ./euca2ools-2.0
 fi
 
 wget http://qa-server/4qa/4_euca2ools/deps/boto-new-latest.tar.gz
-wget http://qa-server/4qa/4_euca2ools/deps/euca2ools-main-latest.tar.gz
+wget http://qa-server/4qa/4_euca2ools/deps/euca2ools-2.0-latest.tar.gz
 
-tar xzf euca2ools-main-latest.tar.gz
+tar xzf euca2ools-2.0-latest.tar.gz
 tar xzf boto-new-latest.tar.gz
 
-rm -f euca2ools-main-latest.tar.gz
+rm -f euca2ools-2.0-latest.tar.gz
 rm -f boto-new-latest.tar.gz
 
-if ! ls -la | grep euca2ools-main; then
+if ! ls -la | grep euca2ools-2.0; then
    exit 1
 fi
 if ! ls -la | grep boto; then
--- stage01/load_win_image.sh
+++ stage01/load_win_image.sh
@@ -3,20 +3,20 @@
 # ../share/windows_images/currentimg
 function download_and_setup_euca2ools {
         dir=$(cat ../input/2b_tested.lst  | grep EUCA2OOLS_PATH | cut -f2 -d '=' | sed 's/[ \r]//g') 
-        if [ -z $dir ] || ! ls ../../$dir/euca2ools-main > /dev/null ; then
+        if [ -z $dir ] || ! ls ../../$dir/euca2ools-2.0 > /dev/null ; then
             echo "Downloading euca2ools"
 	    . ./download_euca2ools.sh
 	    if [ ! $? -eq 0 ]; then
 	   	echo "euca2ools installation failed"
    		exit 1
 	    fi
-	    export PYTHONPATH="$(pwd)/boto:$(pwd)/euca2ools-main"
-	    export PATH="$(pwd)/euca2ools-main/bin:$PATH"
+	    export PYTHONPATH="$(pwd)/boto:$(pwd)/euca2ools-2.0"
+	    export PATH="$(pwd)/euca2ools-2.0/bin:$PATH"
         else
             dir="../../$dir"
             echo "EUCA2OOLS DIR: $dir"
-            export PYTHONPATH=$dir/boto:$dir/euca2ools-main
-            export PATH=$dir/euca2ools-main/bin:$PATH
+            export PYTHONPATH=$dir/boto:$dir/euca2ools-2.0
+            export PATH=$dir/euca2ools-2.0/bin:$PATH
         fi
 	echo "EUCA2OOLS VERSION: $(euca-version)"
 }
